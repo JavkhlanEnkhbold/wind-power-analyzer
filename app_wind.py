aa0f651be7a63a1704ae993a95f808e9765f9ce7
Javkhlan Enkhbold <javkhlan.enkhbold@campus.tu-berlin.de>
2022-07-03 13:56:23 +0200

    Windertrag fertig

--- app_wind.py
+++ app_wind.py
@@ -10,6 +10,7 @@ from yaml import load
 from windrose import WindroseAxes
 from mpl_toolkits.axes_grid.inset_locator import inset_axes
 import math
+import seaborn as sns
 
 
 
@@ -66,10 +67,10 @@ if show_statistic:
 if show_plot:
     st.header("Plotts")
 
-    #Plott1
+    #Plott1. Time Series
     st.subheader("Time-Series")
     fig, ax = plt.subplots()
-    dataframe["Speed"].plot(label="Speed [m/s]")
+    dataframe["Speed"].plot(label="Daily", ylabel="Speed [m/s]")
     daily = dataframe["Speed"].resample('D').mean()
     daily.plot(color="#BB0000", label="Daily Mean")
     ax.legend()
@@ -82,6 +83,7 @@ if show_plot:
     ax.grid(color="black")
     st.pyplot(fig)
     
+    #Plott2: Seasonal Pattern
     fig, ax = plt.subplots(2,6, sharey = True, figsize = (20, 8))
     plt.rcParams["font.size"] = 12
     ax[0,0].plot(dataframe[dataframe.Month == 1]["Speed"],color="darkred")
@@ -139,28 +141,7 @@ if show_plot:
     fig.suptitle(f"Monthly wind speed in 2020 for Berlin")
     st.pyplot(fig)
     
-    
-    #Plott3
-    st.subheader("Wind speed distribution")
-    image = Image.open('/Users/javkhlanenkhbold/Documents/wind-power-analyzer/rawdata/Weibull-Distribution.png')
-    st.image(image, caption='Weibull-Distribution')
-    fig, ax = plt.subplots()
-    dist = stats.weibull_min.fit(daily, floc=0, scale=2)
-    speed_range = np.arange(0, daily.max())
-    ax.plot(stats.weibull_min.pdf(speed_range, *(dist)),
-         color = "blue",
-        label = "Weibull Distribution k = {}".format(round(dist[0], 2)))
-    ax.set_xlabel("Wind speed [m/s]")
-    ax.set_ylabel("Probability")
-    ax.hist(daily, bins=np.linspace(0, daily.max()), edgecolor='black', density=True, stacked=True, alpha=0.5, label="Daily Wind data")
-    ax.grid(color="black", alpha=0.5) 
-    ax.legend()
-    st.pyplot(fig)
-    
-    #Plott4
-
-
-    
+    #Plott3: Windrose
     st.subheader("Windrose")
     plt.figure(figsize = (8, 6))
     ax = WindroseAxes.from_ax()
@@ -186,64 +167,185 @@ if show_plot:
     st.subheader("Wind Turbine")
     st.write("The power curve of a wind turbine is a graph that depicts how much electrical power output is produced by a wind turbine at different wind speeds. These curves are found by field measurements, where the wind speed reading from a device called an anemometer (which is placed on a mast at a reasonable distance to the wind turbine) is read and plotted against the electrical power output from the turbine.")
     
-if type:
-    st.subheader("Anlagentype:" )
-    df = read_units("/Users/javkhlanenkhbold/Documents/wind-power-analyzer/rawdata/supply__wind_turbine_library.csv")
-    df['power_curve_wind_speeds'] = df['power_curve_wind_speeds']
-    df['power_curve_values'] = df['power_curve_values']
+    
+    #Plott4: Anlage
+    if type:
+        st.subheader("Selected unit/modell: {}".format(str(type)) )
+        df = read_units("/Users/javkhlanenkhbold/Documents/wind-power-analyzer/rawdata/supply__wind_turbine_library.csv")
+        df['power_curve_wind_speeds'] = df['power_curve_wind_speeds']
+        df['power_curve_values'] = df['power_curve_values']
+        selected_unit = df.loc[df["name"] == type]
+        
+        lst_wind_speed = selected_unit['power_curve_wind_speeds'].to_list()[0]
+        lst_wind_speed = [float(x.strip(' []')) for x in lst_wind_speed.split(',')]
+        
+        lst_curve_values = selected_unit['power_curve_values'].to_list()[0]
+        lst_curve_values = [float(x.strip(' []')) for x in lst_curve_values.split(',')]
+        
+        lst_merged = [lst_wind_speed, lst_curve_values]
+        power_curve = pd.DataFrame(lst_merged).transpose().set_axis(['wind_speed_class', 'Power at given speed'], axis=1, inplace=False)
+        power_curve.set_index("wind_speed_class", inplace = True)
+        
+        st.write(power_curve)
+        cut_in_speed = 3 
+        cut_out_speed = 25
+        rated_speed = 14
+        rated_power = 810
+        
+        st.subheader("Power Curve")
+        fig, ax = plt.subplots()
+        ax = power_curve["Power at given speed"].plot(color = "darkblue", linewidth = 4, label = "Power Curve")
+        ax.vlines(x = cut_in_speed,
+                ymin = 0,
+                ymax = power_curve.loc[cut_in_speed, "Power at given speed"],
+                linestyle = "dashed",
+                color = "black"
+                )
+        ax.vlines(x = cut_out_speed,
+                ymin = 0,
+                ymax = power_curve.loc[cut_out_speed, "Power at given speed"],
+                color = "darkblue",
+                linewidth = 4
+                )
+        ax.hlines(y = 0,
+                xmin = cut_out_speed,
+                xmax = 30,
+                color = "darkblue",
+                linewidth = 4
+                )
+        plt.xlabel("Speed (m/s)")
+        plt.ylabel("Power at given speed (kW)")
+        plt.title("Power curve diagram of given wind turbine")
+        plt.legend(loc = "upper left")
+        st.pyplot(plt)
+    
+    # Definition of Dataframe
+    df = dataframe
+    data = df.Speed.values
+    params = stats.weibull_min.fit(data,
+                             floc = 0,     #Fix the location at zero
+                             scale = 2    #keeps the first scale parameter of the exponential weibull fixed at once
+                            )
+    
+    df["Speed Class"] = ""
+    for index in df.index:
+        df.loc[index, "Speed Class"] = math.ceil(df.loc[index, "Speed"])
+    
+    st.subheader("Wind Class in details")
     st.write(df)
-    selected_unit = df.loc[df["name"] == type]
     
-    lst_wind_speed = selected_unit['power_curve_wind_speeds'].to_list()[0]
-    lst_wind_speed = [float(x.strip(' []')) for x in lst_wind_speed.split(',')]
+    st.subheader("Frequency in Details")
+    df_new = df.groupby(["Speed Class"]).count()
+    df_new = df_new[["Speed"]]
+    df_new.columns = ["Observed Frequency"]
+    df_new["Cumulative Frequency"] = df_new["Observed Frequency"].cumsum()
+    df_new.sort_index(inplace = True)    
+    st.write(df_new)
     
-    lst_curve_values = selected_unit['power_curve_values'].to_list()[0]
-    lst_curve_values = [float(x.strip(' []')) for x in lst_curve_values.split(',')]
-    
-    lst_merged = [lst_wind_speed, lst_curve_values]
-    power_curve = pd.DataFrame(lst_merged).transpose().set_axis(['wind_speed_class', 'Power at given speed'], axis=1, inplace=False)
-    power_curve.set_index("wind_speed_class", inplace = True)
-    
-    cut_in_speed = 3 
-    cut_out_speed = 25
-    rated_speed = 14
-    rated_power = 810
-    
-    st.write(power_curve)
-    
-    st.subheader("Power Curve")
+    #Plott5: Weibull Verteilung
     fig, ax = plt.subplots()
+    speed_range = np.arange(0, 26)
+    ax.plot(df_new["Observed Frequency"],
+            color = "navy",
+            label = "Observed frequency",
+            linewidth = 3
+        )
+    ax.plot(stats.weibull_min.pdf(speed_range, *params)*len(df),
+            color = "turquoise",
+            label = "Theoretical frequency - Weibull Distr.",
+        linewidth = 3)
 
-    ax = power_curve["Power at given speed"].plot(color = "darkblue", linewidth = 4, label = "Power Curve")
-
-    ax.vlines(x = cut_in_speed,
-            ymin = 0,
-            ymax = power_curve.loc[cut_in_speed, "Power at given speed"],
-            linestyle = "dashed",
-            color = "black"
+    ax.hist(df["Speed"],
+            bins = 21,      #number of bars or classes
+            rwidth = 0.85,    #width of bars
+            edgecolor = "black",
+            color = "cornflowerblue",
+            label = "Wind speed histogram"
             )
+    ax.set_ylabel("Frequency (Number of hours)", color = "navy")
+    ax2 = ax.twinx()
+    ax2.plot(df_new["Cumulative Frequency"],
+            color = "red",
+            label = "Observed Cumulative frequency",
+            linewidth = 3)
 
-    ax.vlines(x = cut_out_speed,
-            ymin = 0,
-            ymax = power_curve.loc[cut_out_speed, "Power at given speed"],
-            color = "darkblue",
-            linewidth = 4
-            )
+    ax2.set_ylabel("Cumulative frequency (number of hours)",
+                color = "red",
+                )
 
-    ax.hlines(y = 0,
-            xmin = cut_out_speed,
-            xmax = 30,
-            color = "darkblue",
-            linewidth = 4
-            )
+    ax2.plot(stats.weibull_min.cdf(speed_range, *params)*len(df),
+            color = "pink",
+            label = "Theoretical Cumulative density",
+            linewidth = 3)
 
-    plt.xlabel("Speed (m/s)")
-    plt.ylabel("Power at given speed (kW)")
-    plt.title("Power curve diagram of given wind turbine")
+    ax.set_xlabel("Speed Class (m/s)")
 
-    #plt.xlim(0, 30)
-    #plt.ylim(0, 1000)
-    plt.legend(loc = "upper left")
+    lines_labels = [ax.get_legend_handles_labels() for ax in fig.axes]
+    lines, labels = [sum(lol, []) for lol in zip(*lines_labels)]
+    lgd = fig.legend(lines, labels, ncol = 2, 
+            bbox_to_anchor = (0.87, 0.05))
+
+    plt.title("Theoretical and observed wind speed distribution in Berlin")
     st.pyplot(plt)
 
+    #Plott6: Power Curve
+    #Enercon E-82/3000    
+    st.subheader("Power Curve")
+    cut_in_speed = 3 
+    cut_out_speed = 25
+    rated_speed = 17
+    rated_power = 3020
     
+    new_row = {"Observed Frequency": [0, 0, 0, 0]}
+    df_new = df_new.append(pd.DataFrame(new_row), ignore_index=True)
+    
+    power_curve["Hours"] = df_new.index.map(df_new["Observed Frequency"])
+    power_curve["Frequency (%)"] = power_curve["Hours"]/power_curve["Hours"].sum()
+    power_curve["Power production distribution"] = power_curve["Frequency (%)"] * power_curve["Power at given speed"]/100
+    power_curve["Energy yield"] = power_curve["Power at given speed"] * power_curve["Hours"]
+    st.write(power_curve)
+    
+    rated_power = 3020
+    capacity_factor = power_curve["Energy yield"].sum() / (power_curve["Hours"].sum() * rated_power)
+    capacity_factor = round(capacity_factor, 2)
+    st.write(f"The capacity factor of the given wind turbine at Berlin in 2020 was ", capacity_factor*100, "%.")
+
+    fig, ax = plt.subplots()
+    ax.plot(stats.weibull_min.pdf(speed_range, *params),
+            color = "blue",
+            label = "Wind speed distribution",
+        linewidth = 2)
+
+    power_curve["Power production distribution"].plot(ax = ax,
+                                                    color = "cornflowerblue",
+                                                    label = "Power production distribution (%)")
+
+    ax.set_ylabel("Frequency (%)", color = "blue")
+
+    ax2 = ax.twinx()
+
+    ax2 = power_curve["Power at given speed"].plot(color = "red", linewidth = 2, label = "Power Curve")
+
+
+    ax2.vlines(x = cut_out_speed,
+            ymin = 0,
+            ymax = power_curve.loc[cut_out_speed, "Power at given speed"],
+            color = "red",
+            linewidth = 2
+            )
+
+    ax2.hlines(y = 0,
+            xmin = cut_out_speed,
+            xmax = 30,
+            color = "red",
+            linewidth = 2
+            )
+
+    ax2.set_ylabel("Power (kW)")
+
+    lines_labels = [ax.get_legend_handles_labels() for ax in fig.axes]
+    lines, labels = [sum(lol, []) for lol in zip(*lines_labels)]
+    fig.legend(lines, labels, ncol = 2, 
+            bbox_to_anchor = (0.87, 0.05))
+
+    st.pyplot(fig)
